391fa953b1287e71083c02aec4e59f916ca91c75
Samuel Torres <42902403+Rilladubz@users.noreply.github.com>
2021-03-09 18:37:49 -0500

    Added redux, set up redux, set up store

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,12 @@
+/**
+ * @function successReducer
+ * @param {array} state - Array of guessed words.
+ * @param {object} action - action to be reduced
+ * @return {boolean} - new success state.
+ */
+
+const success = (state, action) => {
+    return null;
+}
+
+export default success;
